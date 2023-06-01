f8eecf83424fcbe64007157315ee833fecba4a74
JohnOuzounis <menippos06@gmail.com>
2023-06-01 17:53:34 +0300

    feat(): integrate lexer in parser

--- scripts/parser.js
+++ scripts/parser.js
@@ -6,41 +6,58 @@ function parse(input) {
 
     let tokens = lexer.getTokens();
     let currentIndex = 0;
-    let message = 'Parsing Complete';
+    let message = '';
+    let error = false;
+    let line = 1;
 
     function match(token) {
-        if (tokens[currentIndex] === token) {
+        if (currentIndex < tokens.length && tokens[currentIndex].value === token) {
             currentIndex++;
-        } else {
-            let found = tokens[currentIndex];
-            if (currentIndex === tokens.length)
-                found = 'end-of-file';
-            message = `Syntax Error: Expected "${token}" but found "${found}"`;
+            line = (currentIndex >= tokens.length) ? line : tokens[currentIndex].line;
+        }
+        else {
+            let found = (currentIndex === tokens.length) ? 'end-of-file' : tokens[currentIndex].value;
+            message += `Syntax Error (Line ${line}): Expected ${token} but found ${found}\n`;
+            error = true;
         }
     }
 
     function program() {
-        stmts();
+        error = lexer.error;
+        if (!lexer.error) {
+            message += lexer.message;
+            stmts();
+        }
+        else
+            message = lexer.message;
     }
 
     function stmts() {
-        if (tokens[currentIndex] !== ';') {
+        if (currentIndex >= tokens.length || error) return;
+        if (tokens[currentIndex].value !== ';') {
             stmt();
             stmts();
         }
     }
 
     function stmt() {
-        if (tokens[currentIndex] === '"hello"') {
+        if (tokens[currentIndex].value === '"hello"') {
             match('"hello"');
             match(';');
-        } else if (tokens[currentIndex] === ';') {
+        } else if (tokens[currentIndex].value === ';') {
             match(';');
+        }
+        else {
+            let found = (currentIndex === tokens.length) ? 'end-of-file' : tokens[currentIndex].value;
+            message += `Syntax Error (Line ${line}): Unexpected ${tokens[currentIndex].type} ${found} found\n`;
+            error = true;
         }
     }
 
     try {
         program();
+        if (!error)
+            message = 'OK\n';
         return message;
     } catch (error) {
         console.error(error);
