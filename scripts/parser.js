dc45dbea7ee5daaaa1af604a2c10dfeb4c86eacb
JohnOuzounis <menippos06@gmail.com>
2023-06-03 19:35:14 +0300

    refactor(): use nearley to generate parser

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,68 +1,24 @@
-const Lexer = require('./lexer.js');
+const nearley = require('nearley');
+const grammar = require('./grammar.js');
 
 function parse(input) {
-    const lexer = new Lexer(input);
-    lexer.tokenize();
-
-    let tokens = lexer.getTokens();
-    let currentIndex = 0;
     let message = '';
-    let error = false;
-    let line = 1;
+    grammar.Lexer.tokenize(input);
 
-    function match(token) {
-        if (currentIndex < tokens.length && tokens[currentIndex].value === token) {
-            currentIndex++;
-            line = (currentIndex >= tokens.length) ? line : tokens[currentIndex].line;
-        }
-        else {
-            let found = (currentIndex === tokens.length) ? 'end-of-file' : tokens[currentIndex].value;
-            message += `Syntax Error (Line ${line}): Expected ${token} but found ${found}\n`;
-            error = true;
-        }
+    if (grammar.Lexer.error) {
+        message += grammar.Lexer.message;
+        return message;
     }
 
-    function program() {
-        error = lexer.error;
-        if (!lexer.error) {
-            message += lexer.message;
-            stmts();
-        }
-        else
-            message = lexer.message;
-    }
-
-    function stmts() {
-        if (currentIndex >= tokens.length || error) return;
-        if (tokens[currentIndex].value !== ';') {
-            stmt();
-            stmts();
-        }
-    }
-
-    function stmt() {
-        if (tokens[currentIndex].value === '"hello"') {
-            match('"hello"');
-            match(';');
-        } else if (tokens[currentIndex].value === ';') {
-            match(';');
-        }
-        else {
-            let found = (currentIndex === tokens.length) ? 'end-of-file' : tokens[currentIndex].value;
-            message += `Syntax Error (Line ${line}): Unexpected ${tokens[currentIndex].type} ${found} found\n`;
-            error = true;
-        }
-    }
-
+    const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
     try {
-        program();
-        if (!error)
-            message = 'OK\n';
-        return message;
+        parser.feed(grammar.Lexer.getTokens());
+        if (parser.results.length == 0)
+            throw new Error('Syntax Error: Unexpected end of input, reached end of file\n');
     } catch (error) {
-        console.error(error);
-        return message;
+        message += error.message.split('\n')[0];
     }
+    return message;
 }
 
 module.exports = parse;
