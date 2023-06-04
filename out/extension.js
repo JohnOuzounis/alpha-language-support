f5c09288aadc84174a1d7847a9262b722bb98634
JohnOuzounis <menippos06@gmail.com>
2023-06-04 11:56:13 +0300

    feat(): register compile command

--- out/extension.js
+++ out/extension.js
@@ -1,7 +1,10 @@
 "use strict";
 const vscode = require('vscode');
 const parse = require('./parser.js');
+const path = require('path');
+const child_process = require('child_process');
 function activate(context) {
+    const outputChannel = vscode.window.createOutputChannel('Alpha');
     let disposable = vscode.commands.registerCommand('extension.parseGrammar', function () {
         // Get the active text editor
         const editor = vscode.window.activeTextEditor;
@@ -12,8 +15,47 @@ function activate(context) {
             // Parse the selected text
             const result = parse(text);
             // Show the parsing result
-            if (result)
+            if (result) {
                 vscode.window.showErrorMessage(`${result}`);
+                outputChannel.appendLine(`${result}`);
+            }
+        }
+    });
+    let disposableRunCommand = vscode.commands.registerCommand('extension.compileAndRunVM', () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor) {
+            const activeFilePath = activeEditor.document.uri.fsPath;
+            const activeFileName = path.dirname(activeFilePath) + '\\' +
+                path.basename(activeFilePath, path.extname(activeFilePath));
+            const binFolderUri = vscode.Uri.joinPath(context.extensionUri, 'bin');
+            const compilerPath = vscode.Uri.joinPath(binFolderUri, 'alphac.exe').fsPath;
+            const vmPath = vscode.Uri.joinPath(binFolderUri, 'alpha.exe').fsPath;
+            child_process.execFile(`${compilerPath}`, [`${activeFilePath}`], (error, stdout, stderr) => {
+                if (error) {
+                    throw error;
+                }
+                if (stderr) {
+                    outputChannel.appendLine(stderr);
+                    return;
+                }
+                if (stdout) {
+                    outputChannel.appendLine(stdout);
+                    return;
+                }
+            });
+            child_process.execFile(`${vmPath}`, [`${activeFileName}.abc`], (error, stdout, stderr) => {
+                if (error) {
+                    throw error;
+                }
+                if (stderr) {
+                    outputChannel.appendLine(stderr);
+                    return;
+                }
+                if (stdout) {
+                    outputChannel.appendLine(stdout);
+                    return;
+                }
+            });
         }
     });
     // Trigger parsing when a file is saved
@@ -23,11 +65,13 @@ function activate(context) {
             // Parse the entire text of the file
             const result = parse(text);
             // Show the parsing result
-            if (result)
+            if (result) {
                 vscode.window.showErrorMessage(`${result}`);
+                outputChannel.appendLine(`${result}`);
+            }
         }
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable, disposableRunCommand);
 }
 exports.activate = activate;
 function deactivate() { }
--- scripts/extension.js
+++ scripts/extension.js
@@ -1,8 +1,12 @@
 
 const vscode = require('vscode');
 const parse = require('./parser.js');
+const path = require('path');
+const child_process = require('child_process');
 
 function activate(context) {
+    const outputChannel = vscode.window.createOutputChannel('Alpha');
+
     let disposable = vscode.commands.registerCommand('extension.parseGrammar', function () {
         // Get the active text editor
         const editor = vscode.window.activeTextEditor;
@@ -16,8 +20,37 @@ function activate(context) {
             const result = parse(text);
 
             // Show the parsing result
-            if (result)
+            if (result) {
                 vscode.window.showErrorMessage(`${result}`);
+                outputChannel.appendLine(`${result}`);
+            }
+        }
+    });
+
+    let disposableRunCommand = vscode.commands.registerCommand('extension.compileAndRunVM', () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor) {
+            const activeFilePath = activeEditor.document.uri.fsPath;
+            const activeFileName = path.dirname(activeFilePath) + '\\' +
+                path.basename(activeFilePath, path.extname(activeFilePath));
+
+            const binFolderUri = vscode.Uri.joinPath(context.extensionUri, 'bin');
+            const compilerPath = vscode.Uri.joinPath(binFolderUri, 'alphac.exe').fsPath;
+            const vmPath = vscode.Uri.joinPath(binFolderUri, 'alpha.exe').fsPath;
+
+            child_process.execFile(`${compilerPath}`, [`${activeFilePath}`],
+                (error, stdout, stderr) => {
+                    if (error) { throw error }
+                    if (stderr) { outputChannel.appendLine(stderr); return; }
+                    if (stdout) { outputChannel.appendLine(stdout); return; }
+                });
+
+            child_process.execFile(`${vmPath}`, [`${activeFileName}.abc`],
+                (error, stdout, stderr) => {
+                    if (error) { throw error }
+                    if (stderr) { outputChannel.appendLine(stderr); return; }
+                    if (stdout) { outputChannel.appendLine(stdout); return; }
+                });
         }
     });
 
@@ -30,11 +63,13 @@ function activate(context) {
             const result = parse(text);
 
             // Show the parsing result
-            if (result)
+            if (result) {
                 vscode.window.showErrorMessage(`${result}`);
+                outputChannel.appendLine(`${result}`);
+            }
         }
     });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable, disposableRunCommand);
 }
 exports.activate = activate;
 
