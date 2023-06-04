30509a9ffa48c2cef11e2cb669d16d80828ce413
JohnOuzounis <menippos06@gmail.com>
2023-06-04 13:36:25 +0300

    refactor(): use precompiled parser

--- scripts/extension.js
+++ scripts/extension.js
@@ -1,29 +1,24 @@
 
 const vscode = require('vscode');
-const parse = require('./parser.js');
 const path = require('path');
 const child_process = require('child_process');
 
 function activate(context) {
     const outputChannel = vscode.window.createOutputChannel('Alpha');
 
-    let disposable = vscode.commands.registerCommand('extension.parseGrammar', function () {
-        // Get the active text editor
-        const editor = vscode.window.activeTextEditor;
+    let disposableParseCommand = vscode.commands.registerCommand('extension.parseGrammar', () => {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor) {
+            const activeFilePath = activeEditor.document.uri.fsPath;
+            const binFolderUri = vscode.Uri.joinPath(context.extensionUri, 'bin');
+            const parserPath = vscode.Uri.joinPath(binFolderUri, 'parser.exe').fsPath;
 
-        if (editor) {
-            // Get the selected text
-            const selection = editor.selection;
-            const text = editor.document.getText(selection);
-
-            // Parse the selected text
-            const result = parse(text);
-
-            // Show the parsing result
-            if (result) {
-                vscode.window.showErrorMessage(`${result}`);
-                outputChannel.appendLine(`${result}`);
-            }
+            child_process.execFile(`${parserPath}`, [`${activeFilePath}`],
+                (error, stdout, stderr) => {
+                    if (error) { outputChannel.appendLine(stderr); throw error }
+                    if (stderr) { outputChannel.appendLine(stderr); return; }
+                    if (stdout) { outputChannel.appendLine(stdout); return; }
+                });
         }
     });
 
@@ -40,14 +35,14 @@ function activate(context) {
 
             child_process.execFile(`${compilerPath}`, [`${activeFilePath}`],
                 (error, stdout, stderr) => {
-                    if (error) { throw error }
+                    if (error) { outputChannel.appendLine(stderr); throw error }
                     if (stderr) { outputChannel.appendLine(stderr); return; }
                     if (stdout) { outputChannel.appendLine(stdout); return; }
                 });
 
             child_process.execFile(`${vmPath}`, [`${activeFileName}.abc`],
                 (error, stdout, stderr) => {
-                    if (error) { throw error }
+                    if (error) { outputChannel.appendLine(stderr); throw error }
                     if (stderr) { outputChannel.appendLine(stderr); return; }
                     if (stdout) { outputChannel.appendLine(stdout); return; }
                 });
@@ -57,19 +52,22 @@ function activate(context) {
     // Trigger parsing when a file is saved
     vscode.workspace.onDidSaveTextDocument((document) => {
         if (document.languageId === 'alpha') {
-            const text = document.getText();
+            const activeEditor = vscode.window.activeTextEditor;
+            if (activeEditor) {
+                const activeFilePath = activeEditor.document.uri.fsPath;
+                const binFolderUri = vscode.Uri.joinPath(context.extensionUri, 'bin');
+                const parserPath = vscode.Uri.joinPath(binFolderUri, 'parser.exe').fsPath;
 
-            // Parse the entire text of the file
-            const result = parse(text);
-
-            // Show the parsing result
-            if (result) {
-                vscode.window.showErrorMessage(`${result}`);
-                outputChannel.appendLine(`${result}`);
+                child_process.execFile(`${parserPath}`, [`${activeFilePath}`],
+                    (error, stdout, stderr) => {
+                        if (error) { outputChannel.appendLine(stderr); throw error }
+                        if (stderr) { outputChannel.appendLine(stderr); return; }
+                        if (stdout) { outputChannel.appendLine(stdout); return; }
+                    });
             }
         }
     });
-    context.subscriptions.push(disposable, disposableRunCommand);
+    context.subscriptions.push(disposableParseCommand, disposableRunCommand);
 }
 exports.activate = activate;
 
