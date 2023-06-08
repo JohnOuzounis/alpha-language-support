c5aa20c44e141c082b16e04abdfee0551bcba4ad
JohnOuzounis <menippos06@gmail.com>
2023-06-08 19:06:52 +0300

    refactor(): reuse code

--- scripts/completion.js
+++ scripts/completion.js
@@ -5,70 +5,26 @@ class AlphaCompletionItemProvider {
     provideCompletionItems(document, position, token) {
         const completionItems = [];
 
-        var variableItem = new vscode.CompletionItem('print', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function print';
-        variableItem.insertText = 'print';
-        completionItems.push(variableItem);
+        function makeItem(name, type) {
+            var item = new vscode.CompletionItem(name, type);
+            item.detail = `Library function ${name}`;
+            item.insertText = name;
+            return item;
+        }
 
-        var variableItem = new vscode.CompletionItem('println', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function println';
-        variableItem.insertText = 'println';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('sin', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function sin';
-        variableItem.insertText = 'sin';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('cos', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function cos';
-        variableItem.insertText = 'cos';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('sqrt', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function sqrt';
-        variableItem.insertText = 'sqrt';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('typeof', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function typeof';
-        variableItem.insertText = 'typeof';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('totalarguments', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function totalarguments';
-        variableItem.insertText = 'totalarguments';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('argument', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function argument';
-        variableItem.insertText = 'argument';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('strtonum', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function strtonum';
-        variableItem.insertText = 'strtonum';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('input', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function input';
-        variableItem.insertText = 'input';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('objecttotalmembers', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function objecttotalmembers';
-        variableItem.insertText = 'objecttotalmembers';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('objectcopy', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function objectcopy';
-        variableItem.insertText = 'objectcopy';
-        completionItems.push(variableItem);
-
-        var variableItem = new vscode.CompletionItem('objectmemberkeys', vscode.CompletionItemKind.Function);
-        variableItem.detail = 'Library function objectmemberkeys';
-        variableItem.insertText = 'objectmemberkeys';
-        completionItems.push(variableItem);
+        completionItems.push(makeItem('print', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('println', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('sin', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('cos', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('sqrt', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('typeof', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('totalarguments', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('argument', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('strtonum', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('input', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('objecttotalmembers', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('objectcopy', vscode.CompletionItemKind.Function));
+        completionItems.push(makeItem('objectmembrekeys', vscode.CompletionItemKind.Function));
 
         return Promise.resolve(completionItems);
     }
